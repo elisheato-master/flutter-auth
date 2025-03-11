415d812d048015207266abed88a7e3ba61f7edbe
Elisheato <49386065+elisheato-master@users.noreply.github.com>
2025-03-12 07:28:00 +0800

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+// lib/main.dart
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'screens/login_screen.dart';
+import 'services/auth_service.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (_) => AuthService(),
+      child: MaterialApp(
+        title: 'Flutter Auth App',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: const LoginScreen(),
+        debugShowCheckedModeBanner: false,
+      ),
+    );
+  }
+}
