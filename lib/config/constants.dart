6d9bbe4bfa1546e6df7113704dfe6c04ac064f64
Elisheato <49386065+elisheato-master@users.noreply.github.com>
2025-03-12 07:29:00 +0800

    Create constants.dart

---
+++ lib/config/constants.dart
@@ -0,0 +1,8 @@
+// lib/config/constants.dart
+class Constants {
+  // MongoDB connection string
+  static const String mongoDbUri = 'mongodb://username:password@localhost:27017/auth_app';
+  
+  // Collection name
+  static const String usersCollection = 'users';
+}
